01960d448bc1ef7edda37ddfe72472dc70adbdca
Lucas <lucaslavratti98@gmail.com>
2022-04-27 02:58:01 -0300

    Styling

--- src/app.py
+++ src/app.py
@@ -13,19 +13,18 @@ for i in range(len(nodes["name"])):
     my_elements.append(
         {
             "data": {"id": str(nodes["id"][i]), "label": str(nodes["name"][i])},
-            #"classes": [nodes["type"][i], "node-w-{}".format(nodes["weight"][i]),]
+            "classes": "{} node-w-{}".format(nodes["type"][i], nodes["weight"][i])
         }
     )
 for i in range(len(graph["from"])):
     my_elements.append(
         {
             "data": {"source": str(graph["from"][i]), "target": str(graph["to"][i])},
-            #"classes": "edge-w-{}".format(graph["weight"][i]),
+            "classes": "edge-w-{}".format(graph["weight"][i]),
         }
     )
 
-
-# cyto.load_extra_layouts()
+cyto.load_extra_layouts()
 
 app = dash.Dash(__name__)
 app.layout = html.Div(
@@ -45,7 +44,43 @@ app.layout = html.Div(
             id="cytoscape",
             layout={"name": "cose"},
             elements=my_elements,
-
+            minZoom=0.1,
+            maxZoom=20,
+            style={'width': '100%', 'height': '700px'},
+            stylesheet=[
+                        
+                        # Class selectors
+                        {
+                            'selector': '.acao',
+                            'style': {
+                                'shape': 'square',
+                                'background-color': 'orange',
+                                'line-color': 'orange'
+                            }
+                        },
+                        {
+                            'selector': '.envolvido',
+                            'style': {
+                                'shape': 'circle',
+                                'background-color': 'gray',
+                                'line-color': 'gray',
+                                'color': 'black',
+                                'text-valign' : 'center',
+                                'text-halign' : 'center',
+                                'label': 'data(label)',
+                                'font-size': '2px'
+                                
+                            }
+                        },
+                        {
+                            'selector': '[label *= "Luiz Inácio"]',
+                            'style': {
+                                'shape': 'star',
+                                'background-color': 'red',
+                                'line-color': 'red',                                
+                            }
+                        }
+                    ]
         ),
         html.Div(
             [
